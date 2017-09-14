6df9e6b758de8344be2f04dcdb2a85e7c0ae48d6
21guns <liujun_0810@163.com>
2017-09-14 18:41:11 +0800

    feature:add sub package

---
+++ user.controller/src/main/java/com/guns21/user/controller/UserController.java
@@ -0,0 +1,17 @@
+package com.guns21.user.controller;
+
+
+import com.guns21.support.controller.AppBaseController;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Created by jliu on 16/6/30.
+ */
+@RestController
+@RequestMapping("/app/user/v1")
+public class UserController extends AppBaseController {
+
+
+
+}
